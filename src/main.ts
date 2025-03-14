f25cf9bd7245d4bc221bf7d180201888de73e0ad
jayudoye <joeludoye@gmail.com>
2025-03-14 10:29:53 +0100

    chore: Update main.ts to enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import express from 'express';
-import cors from 'cors';
 
-const app = express()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.use(cors());
+  app.enableCors();
   
   await app.listen(process.env.PORT);
 }
