5999a008519996da7a52670fd79212bd858ccdac
jayudoye <joeludoye@gmail.com>
2025-03-14 13:18:28 +0100

    chore: Update package.json file and dependency to enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.enableCors();
+  app.use(cors());
   
   await app.listen(process.env.PORT);
 }
