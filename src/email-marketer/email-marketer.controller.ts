8e80d0854f609295b927d1d9202c51671ccb1333
jayudoye <joeludoye@gmail.com>
2025-03-21 09:20:52 +0100

    refactor: centralize configuration using envConfig for port and server URL

--- src/email-marketer/email-marketer.controller.ts
+++ src/email-marketer/email-marketer.controller.ts
@@ -7,6 +7,7 @@ import {
   Post,
 } from '@nestjs/common';
 import { EmailMarketerService } from './email-marketer.service';
+import { ENV_CONFIG } from "../utils/envConfig";
 
 @Controller('email-marketer')
 export class EmailMarketerController {
@@ -57,7 +58,7 @@ export class EmailMarketerController {
           app_description:
             'An AI-powered email marketing agent designed to generate engaging and personalized marketing emails',
           app_logo: 'https://cdn-icons-png.flaticon.com/512/7286/7286142.png',
-          app_url: 'https://mastraaiemailagent.onrender.com/',
+          app_url: ENV_CONFIG.SERVER_URL,
           background_color: '#fff',
         },
         is_active: true,
@@ -80,9 +81,9 @@ export class EmailMarketerController {
           },
         ],
         target_url:
-          'https://mastraaiemailagent.onrender.com/email-marketer/generate',
+          `${ENV_CONFIG.SERVER_URL}/email-marketer/generate`,
         tick_url:
-          'https://mastraaiemailagent.onrender.com/email-marketer/generate/integration-config',
+          `${ENV_CONFIG.SERVER_URL}/email-marketer/generate/integration-config`,
       },
     };
   }
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
+import { ENV_CONFIG } from "./utils/envConfig";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors();
-  
-  await app.listen(process.env.PORT || 3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+
+  await app.listen(ENV_CONFIG.PORT, () => {
+    console.log(`Application running on port ${ENV_CONFIG.PORT}`);
+  })
 }
 bootstrap();
