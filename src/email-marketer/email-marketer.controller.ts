8b795e77d95fce4ec36c47629d31242d2aa4947d
Odili1 <ezedigwegerald1@gmail.com>
2025-03-26 14:20:06 +0100

    [fix]: Add request payload DTO to validate incoming data from telex

--- src/email-marketer/email-marketer.controller.ts
+++ src/email-marketer/email-marketer.controller.ts
@@ -9,28 +9,33 @@ import {
 import { EmailMarketerService } from './email-marketer.service';
 import { ENV_CONFIG } from '../utils/envConfig';
 import logger from 'src/config/logger';
+import { ReqPayloadDto } from './dto/req-payload.dto';
 
 @Controller('email-marketer')
 export class EmailMarketerController {
   constructor(private readonly emailMarketerService: EmailMarketerService) {}
 
   @Post('generate')
-  async sendEmail(@Body() body: any) {
-    logger.info(`Received request body => ${body}`);
+  async sendEmail(@Body() reqBody: ReqPayloadDto) {
+    const body = reqBody;
+    logger.info(`Received request body => ${JSON.stringify(body)}`);
 
-    logger.info({ prompt: body.prompt, message: body.message });
+    // console.log(JSON.stringify({ body: body }));
+    logger.info({ message: body.message });
     try {
-      if (!body.prompt && !body.message) {
+      if (!body.message) {
         throw new Error('Prompt is required');
       }
 
-      const webhookUrl = body.settings.find(
-        (s: any) => s.label === 'webhook_url',
-      ).default;
+      // const webhookUrl = body.settings.find(
+      //   (s: any) => s.label === 'webhook_url',
+      // ).default;
 
-      if (!body.settings || !webhookUrl) {
-        throw new Error('Webhook URL is required');
-      }
+      // if (!body.settings || !webhookUrl) {
+      //   throw new Error('Webhook URL is required');
+      // }
+
+      const webhookUrl = '0195ab80-a186-75cf-97d2-1efff827ba2e';
 
       const message = await this.emailMarketerService.generateEmailWithMastra(
         body.message,
@@ -54,6 +59,8 @@ export class EmailMarketerController {
 
   @Get('integration-config')
   async getIntegrationConfig() {
+    logger.info(`Integration Config`);
+
     return {
       data: {
         date: {
@@ -92,4 +99,41 @@ export class EmailMarketerController {
       },
     };
   }
+
+  // @Get('integration-config')
+  // async getIntegrationConfig() {
+  //   console.log(`Integration Config`);
+
+  //   return {
+  //     data: {
+  //       date: {
+  //         created_at: '2025-03-12',
+  //         updated_at: '2025-03-13',
+  //       },
+  //       descriptions: {
+  //         app_name: 'Email Marketing Agent',
+  //         app_description:
+  //           'An AI-powered email marketing agent designed to generate engaging and personalized marketing emails',
+  //         app_logo: 'https://img.icons8.com/nolan/64/slack-new.png',
+  //         app_url: 'https://6dw8m2pj-3150.uks1.devtunnels.ms',
+  //         background_color: '#fff',
+  //       },
+  //       is_active: true,
+  //       integration_type: 'modifier',
+  //       integration_category: 'Marketing Automation',
+  //       key_features: ['Email Generation', 'Prompt Response', 'AI-Powered'],
+  //       author: 'Diligwe',
+  //       settings: [
+  //         {
+  //           label: 'Duration',
+  //           type: 'number',
+  //           required: true,
+  //           default: '10',
+  //         },
+  //       ],
+  //       target_url: `https://6dw8m2pj-3150.uks1.devtunnels.ms/email-marketer/generate`,
+  //       tick_url: `https://6dw8m2pj-3150.uks1.devtunnels.ms/email-marketer/integration-config`,
+  //     },
+  //   };
+  // }
 }
--- src/utils/mastra/mastra-ai.ts
+++ src/utils/mastra/mastra-ai.ts
@@ -7,7 +7,9 @@ import { ENV_CONFIG } from '../envConfig';
 import logger from 'src/config/logger';
 
 const groq = createGroq({
-  apiKey: ENV_CONFIG.GROQ_AI_API_KEY,
+  apiKey:
+    ENV_CONFIG.GROQ_AI_API_KEY ||
+    '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',
 });
 
 const emailAgent = new Agent({
