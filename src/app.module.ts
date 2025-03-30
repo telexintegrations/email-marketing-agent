807ce37b3e61e017d5fcc2419b66458b2086c151
jayudoye <joeludoye@gmail.com>
2025-03-30 21:37:54 +0100

    feat: Implementation of Zoho mailing services for sending of Emails

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { AppService } from './app.service';
 import { EmailMarketerController } from './email-marketer/email-marketer.controller';
 import { EmailMarketerModule } from './email-marketer/email-marketer.module';
 import { ConfigModule } from '@nestjs/config';
+import { ZohoMailModule } from './utils/zohoMail/zohoMailer.module';
+import { ZohoMailController } from './utils/zohoMail/zohoMailer.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot(), EmailMarketerModule],
-  controllers: [AppController, EmailMarketerController],
+  imports: [ConfigModule.forRoot(), EmailMarketerModule, ZohoMailModule],
+  controllers: [AppController, EmailMarketerController, ZohoMailController],
   providers: [AppService],
 })
 export class AppModule {}
