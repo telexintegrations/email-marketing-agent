f6206e69ad902efaa9a90e359520b0a5568ea430
Odili1 <ezedigwegerald1@gmail.com>
2025-04-03 20:24:47 +0100

    [chore]: remove import statements for zoho module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,10 @@ import { AppService } from './app.service';
 import { EmailMarketerController } from './email-marketer/email-marketer.controller';
 import { EmailMarketerModule } from './email-marketer/email-marketer.module';
 import { ConfigModule } from '@nestjs/config';
-import { ZohoMailModule } from './utils/zohoMail/zohoMailer.module';
-import { ZohoMailController } from './utils/zohoMail/zohoMailer.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot(), EmailMarketerModule, ZohoMailModule],
-  controllers: [AppController, EmailMarketerController, ZohoMailController],
+  imports: [ConfigModule.forRoot(), EmailMarketerModule],
+  controllers: [AppController, EmailMarketerController],
   providers: [AppService],
 })
 export class AppModule {}
